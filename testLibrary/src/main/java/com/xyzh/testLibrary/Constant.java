9155e8781568f53a542aef69cd576c9d40107bf6
tanguowei <>
2018-12-27 17:33:37 +0800

    增加源码

---
+++ testLibrary/src/main/java/com/xyzh/testLibrary/Constant.java
@@ -0,0 +1,14 @@
+package com.xyzh.testLibrary;
+
+/**
+ * 作者：liji on 2018/1/22 09:46
+ * 邮箱：lijiwork@sina.com
+ * QQ ：275137657
+ */
+
+public class Constant {
+    public static final String CITY_DATA = "china_city_data.json";
+    public static final String CITY_DATA2 = "china_city_data.json2";
+    public static final String CITY_DATA3 = "china_city_data.json3";
+    public static final String CITY_DATA6 = "china_city_data.json6";
+}
